a18f385adf870d153b56796ccec943049f34a191
Christian Hernandez <chernandez148@DESKTOP-3F6UKNT.localdomain>
2023-03-29 09:43:25 -0700

    updated

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,1 +1,4 @@
-etyuer
+from sqlalchemy import create_engine, func
+from sqlalchemy import ForeignKey, Column, Integer, String
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
