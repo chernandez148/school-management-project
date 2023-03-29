b8502ff4ab527db53ed00c2e34121774069232e4
chernandez148 <66753093+chernandez148@users.noreply.github.com>
2023-03-29 11:08:13 -0700

    Update seed.py

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -1,64 +1,48 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, func
+from sqlalchemy import ForeignKey, Column, Integer, String
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
 
-from models import Teachers, Students, Grades
+
+engine = create_engine('sqlite:///school.db')
+
+Base = declarative_base()
 
 
-if __name__ == '__main__':
-    engine = create_engine('sqlite:///school.db')
-    Session = sessionmaker(bind=engine)
-    session = Session()
+class Teachers(Base):
+    __tablename__ = "teachers"
 
-    # DELETE TEACHERS,STUDENTS, and GRADES; CLEAR DATA ON INITIATION
-    session.query(Teachers).delete()
-    session.query(Students).delete()
-    session.query(Grades).delete()
+    id = Column(Integer(), primary_key=True)
+    first_name = Column(String())
+    last_name = Column(String())
+    subject = Column(String())
 
-    teacher1 = Teachers(first_name="Regina",
-                        last_name="Tran", subject="Science")
-    teacher2 = Teachers(first_name="Juan", last_name="Chacon", subject="Math")
-    teacher3 = Teachers(first_name="Lisa",
-                        last_name="Cardoso", subject="History")
+    grades = relationship('Grades', backref=backref('teachers'))
 
-    session.add(teacher1)
-    session.add(teacher2)
-    session.add(teacher3)
-    session.commit()
+    def __repr__(self):
+        return f"Teacher(id={self.id}, name='{self.first_name} {self.last_name}', subject='{self.subject}')"
 
-    student1 = Students(first_name='Billy', last_name="Bob")
-    student2 = Students(first_name='Hannah', last_name='Smith')
-    student3 = Students(first_name='George', last_name='Richards')
-    student4 = Students(first_name='Sarah', last_name='Connor')
 
-    session.add(student1)
-    session.add(student2)
-    session.add(student3)
-    session.add(student4)
-    session.commit()
+class Students(Base):
+    __tablename__ = 'students'
 
-    billys_grade_science = Grades(teachers_id=1, students_id=1, grade="B")
-    billys_grade_math = Grades(teachers_id=2, students_id=1, grade="B")
+    id = Column(Integer(), primary_key=True)
+    first_name = Column(String())
+    last_name = Column(String())
 
-    hannahs_grade_science = Grades(teachers_id=1, students_id=2, grade='A')
-    hannahs_grade_math = Grades(teachers_id=2, students_id=2, grade="B")
-    hannahs_grade_history = Grades(teachers_id=3, students_id=2, grade='C')
+    grades = relationship('Grades', backref=backref('students'))
 
-    george_grade_science = Grades(teachers_id=1, students_id=3, grade='C')
-    george_grade_math = Grades(teachers_id=2, students_id=3, grade="B")
-    george_grade_history = Grades(teachers_id=3, students_id=3, grade='B')
+    def __repr__(self):
+        return f"Student(id={self.id}, name='{self.first_name} {self.last_name}')"
 
-    sarah_grade_math = Grades(teachers_id=2, students_id=4, grade="B")
 
-    session.add(billys_grade_science)
-    session.add(billys_grade_math)
+class Grades(Base):
+    __tablename__ = "grades"
 
-    session.add(hannahs_grade_science)
-    session.add(hannahs_grade_math)
-    session.add(hannahs_grade_history)
+    id = Column(Integer(), primary_key=True)
+    teachers_id = Column(Integer(), ForeignKey('teachers.id'))
+    students_id = Column(Integer(), ForeignKey('students.id'))
+    grade = Column(String())
 
-    session.add(george_grade_science)
-    session.add(george_grade_math)
-    session.add(george_grade_history)
-
-    session.add(sarah_grade_math)
-    session.commit()
+    def __repr__(self):
+        return f"Grade(id={self.id}, teacher_id={self.teachers_id}, student_id={self.students_id}, grade='{self.grade}')"
